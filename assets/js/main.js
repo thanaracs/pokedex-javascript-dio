9c0c5d9f55f4a2143aff4a206449ad0bca874c48
Thainara Carvalho <thai.cds1@gmail.com>
2022-11-07 13:31:39 -0300

    iniciando com o CCS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,18 @@
 
-function convertPokemonToLi(pokemon){
-    return `<li class="pokemon">
-    <span class="number">#${pokemon.number}</span>
-    <span class="name">${pokemon.name}</span>
-    <div class="detail">
-        <ol class="types">
-            ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
-        </ol>
-        <img src="${pokemon.photo}" alt="imagem ${pokemon.name}">
-    </div>
-</li>`
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `
 }
 
 const pokemonList = document.getElementById('pokemonList')
